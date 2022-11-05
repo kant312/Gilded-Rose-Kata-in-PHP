3dc3b7d05d5624d001ec66c84c2728e9a92ea297
Quentin Delcourt <qdelcourt@optimy.com>
2022-11-05 22:45:13 +0100

    Use constants instead of magic strings

--- tests/App/GildedRoseTest.php
+++ tests/App/GildedRoseTest.php
@@ -38,24 +38,24 @@ class GildedRoseTest extends TestCase
 
     public function test_quality_varies_for_backstage_convert()
     {
-        $gildedRose = GildedRose::of(...$this->makeItem(4, 11, 'Backstage passes to a TAFKAL80ETC concert'));
+        $gildedRose = GildedRose::of(...$this->makeItem(4, 11, ItemFactory::BACKSTAGE_CONCERT));
         $gildedRose->tick();
         self::assertEquals(5, $gildedRose->item()->getQuality());
         $gildedRose->tick();
         self::assertEquals(7, $gildedRose->item()->getQuality());
 
-        $gildedRose = GildedRose::of(...$this->makeItem(4, 5, 'Backstage passes to a TAFKAL80ETC concert'));
+        $gildedRose = GildedRose::of(...$this->makeItem(4, 5, ItemFactory::BACKSTAGE_CONCERT));
         $gildedRose->tick();
         self::assertEquals(7, $gildedRose->item()->getQuality());
 
-        $gildedRose = GildedRose::of(...$this->makeItem(4, 0, 'Backstage passes to a TAFKAL80ETC concert'));
+        $gildedRose = GildedRose::of(...$this->makeItem(4, 0, ItemFactory::BACKSTAGE_CONCERT));
         $gildedRose->tick();
         self::assertEquals(0, $gildedRose->item()->getQuality());
     }
 
     public function test_aged_brie_increases_in_quality_as_it_ages(): void
     {
-        $gildedRose = GildedRose::of(...$this->makeItem(4, 2, 'Aged Brie'));
+        $gildedRose = GildedRose::of(...$this->makeItem(4, 2, ItemFactory::AGED_BRIED));
         $gildedRose->tick();
         self::assertEquals(5, $gildedRose->item()->getQuality());
     }
@@ -78,14 +78,14 @@ class GildedRoseTest extends TestCase
 
     public function test_quality_never_goes_above_50(): void
     {
-        $gildedRose = GildedRose::of(...$this->makeItem(50, 1, 'Aged Brie'));
+        $gildedRose = GildedRose::of(...$this->makeItem(50, 1, ItemFactory::AGED_BRIED));
         $gildedRose->tick();
         self::assertEquals(50, $gildedRose->item()->getQuality());
     }
 
     public function test_sulfuras_never_have_to_be_sold_and_keep_their_quality()
     {
-        $gildedRose = GildedRose::of(...$this->makeItem(80,5, 'Sulfuras, Hand of Ragnaros'));
+        $gildedRose = GildedRose::of(...$this->makeItem(80,5, ItemFactory::SULFURAS_HAND_OF_RAGNAROS));
         $gildedRose->tick();
         self::assertEquals(80, $gildedRose->item()->getQuality());
         self::assertEquals(5, $gildedRose->item()->getSellIn());
@@ -93,7 +93,7 @@ class GildedRoseTest extends TestCase
 
     public function test_backstage_passes_increase_in_quality_as_sell_in_decreases()
     {
-        $gildedRose = GildedRose::of(...$this->makeItem(1, 1, 'Backstage passes to a TAFKAL80ETC concert'));
+        $gildedRose = GildedRose::of(...$this->makeItem(1, 1, ItemFactory::BACKSTAGE_CONCERT));
         $gildedRose->tick();
         self::assertEquals(4, $gildedRose->item()->getQuality());
     }
